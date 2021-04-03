3731a133b0f6082e8258c9de45aec0aad5a58dd3
Ampflower 🌺 <git@ampflower.gay>
2021-04-03 07:31:20 -0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * @author Ampflower
+ * @since Nov. 12, 2020 @ 02:27
+ **/
+module gay.ampflower.hachimitsu.utilities {
+    //noinspection JavaRequiresAutoModule
+    requires transitive org.jetbrains.annotations;
+    exports gay.ampflower.hachimitsu.utilities;
+}
