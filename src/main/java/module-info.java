5f342c76cad8f725a17e8fabac19c07906d8d7e0
Ampflower 🌺 <git@ampflower.gay>
2022-07-17 22:29:16 -0500

    Use tabs for formatting with .editorconfig

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@
  * @since Nov. 12, 2020 @ 02:27
  **/
 module gay.ampflower.hachimitsu.utilities {
-    //noinspection JavaRequiresAutoModule
-    requires transitive org.jetbrains.annotations;
-    exports gay.ampflower.hachimitsu.utilities;
+	//noinspection JavaRequiresAutoModule
+	requires transitive org.jetbrains.annotations;
+	exports gay.ampflower.hachimitsu.utilities;
 }
